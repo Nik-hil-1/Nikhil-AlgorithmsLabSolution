8eafea5fc61fcf41a6d46e53e894ca279e3b70e4
Nik-hil-1 <87601589+Nik-hil-1@users.noreply.github.com>
2021-09-20 14:50:22 +0530

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -1,31 +1,29 @@
-package com.gl.lab.currencydenomination;
+package com.gl.lab.paymoney;
 
 import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Enter the size of the currency denomination");
+		System.out.println("Enter the size of the transaction array!");
 		int size = sc.nextInt();
-
-		int[] denomination = new int[size];
-
+		int[] transactions = new int[size];
 		for (int i = 0; i < size; i++) {
-			System.out.println("\nEnter the " + (i + 1) + " denomination");
-			denomination[i] = sc.nextInt();
+			System.out.println("Enter " + (i + 1) + " transaction");
+			transactions[i] = sc.nextInt();
 		}
 
-		System.out.println("\nEnter the price to pay");
-		int price = sc.nextInt();
-
-		CurrencyDenomination currency = new CurrencyDenomination();
-		MergeSort mergeSort = new MergeSort();
-		mergeSort.sort(denomination, 0, denomination.length - 1);
-		currency.noteCounter(denomination, price);
+		System.out.println("Enter the target to achieve!");
+		int target = sc.nextInt();
+		PayMoney payMoney = new PayMoney();
+		int no_of_days = payMoney.no_of_days(transactions, target);
+		if (no_of_days == -1)
+			System.out.println("Target is not acheived.");
+		else
+			System.out.println("Target achieved after " + no_of_days + " transaction.");
 
 		sc.close();
 	}
+
 }
